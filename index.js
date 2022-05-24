4329f19dc76410a867d76a5bceb655c49c9b30e8
muhayminul <muhayminul96@gmail.com>
2022-05-24 23:47:19 +0600

    hello server is added

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
 const express = require("express");
 const app = express();
-   var cors = require('cors')
+require("dotenv").config();
+var cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const port = process.env.PORT || 5000;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@carpenter-haven.3lfjr.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    console.log("connect");
+    // for database connection
+
+    const productCollection = client.db("carpenter").collection("products");
+
+    // get all product
+
+    app.get("/products", async (req, res) => {
+      const query = {};
+      const cursor = await productCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+  } finally {
+    // await client.close();
+  }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("hello man");
@@ -15,4 +46,3 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log("runing curd");
 });
-
