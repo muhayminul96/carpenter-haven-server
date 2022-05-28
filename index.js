9949ef268d1c4e39359a47c75429243dd76a6ded
muhayminul <muhayminul96@gmail.com>
2022-05-28 16:23:02 +0600

    delete is updated

--- index.js
+++ index.js
@@ -68,6 +68,14 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/order/:id", async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await orderCollection.deleteOne(query);
+        res.send(result)
+        
+      });
+
     app.get("/myorders", async (req, res) => {
       const email = req.query.email;
       console.log(email);
